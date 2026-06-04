b4b55e45b7c91cc58e57f4ef2ed4819f0f56e08d
zmacken <hannah.zemack@gmail.com>
2026-06-04 12:35:03 +0000

    small changes

--- Marathos/transformations/gold/marts/mart_country_representation.sql
+++ Marathos/transformations/gold/marts/mart_country_representation.sql
@@ -6,9 +6,10 @@ SELECT
   count(DISTINCT r.athlete_id)  AS antal_unika_lopare,
   count(*)                      AS antal_starter,
   round(
-    count(DISTINCT r.athlete_id) * 100.0 / sum(count(DISTINCT r.athlete_id)) OVER (), 2
+  count(DISTINCT r.athlete_id) * 100.0 / sum(count(DISTINCT r.athlete_id)) OVER (), 2
   ) AS andel_procent --kanske ta bort
 FROM marathos.gold.fct_results r
+
 JOIN marathos.gold.dim_athlete a ON r.athlete_id = a.athlete_id
 GROUP BY a.athlete_country_name, a.athlete_country
 ORDER BY antal_unika_lopare DESC;
